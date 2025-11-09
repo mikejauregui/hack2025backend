c1b888dc5e82f018252dbeab0c59be205d35e971
Iddar Olivares <iddar@dbug.mx>
2025-11-09 08:55:20 -0600

    Update access token value in makePayment function

--- src/payment-partial.ts
+++ src/payment-partial.ts
@@ -47,7 +47,7 @@ export async function makePayment(amount: number) {
     resourceServer:
       "https://ilp.interledger-test.dev/f537937b-7016-481b-b655-9f0d1014822c",
     access_token: {
-      value: "F1DCA096179496874E6E",
+      value: "82CFD84CA33858BE2332",
     },
   };
 
